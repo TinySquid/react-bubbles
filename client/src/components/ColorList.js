525f427b82405330d93b466ec92ac9e17a4e9317
Michael Nunes <miketnunes@gmail.com>
2019-11-15 08:57:13 -0800

    Add edit color functionality, MVP reached

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -17,10 +17,20 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
-    console.log('edit', colorToEdit)
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(response => {
+        console.log(response)
+        console.log(colors)
+        updateColors(colors.map(color => {
+          if (color.id === response.data.id) {
+            return response.data
+          } else {
+            return color;
+          }
+        }));
+      })
+      .catch(error => console.log(error));
   };
 
   const deleteColor = color => {
